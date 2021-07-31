cdee2b0d2a44d6e3c25e85e3bf89b4fa7bbea2b1
unrealserg <serg.taturin@gmail.com>
2021-07-31 23:55:16 +0300

    Каркас приложения

--- js/main.js
+++ js/main.js
@@ -1,23 +1,51 @@
-new Swiper('.swiper-container', {
-	loop: true,
-	navigation: {
-		nextEl: '.arrow',
-	},
-	breakpoints: {
-		320: {
-			slidesPerView: 1,
-			spaceBetween: 20
-		},
-		541: {
-			slidesPerView: 2,
-			spaceBetween: 40
-		}
+// new Swiper('.swiper-container', {
+// 	loop: true,
+// 	navigation: {
+// 		nextEl: '.arrow',
+// 	},
+// 	breakpoints: {
+// 		320: {
+// 			slidesPerView: 1,
+// 			spaceBetween: 20
+// 		},
+// 		541: {
+// 			slidesPerView: 2,
+// 			spaceBetween: 40
+// 		}
+// 	}
+// });
+
+// const menuButton = document.querySelector('.menu-button');
+// const menu = document.querySelector('.header');
+// menuButton.addEventListener('click', function () {
+// 	menuButton.classList.toggle('menu-button-active');
+// 	menu.classList.toggle('header-active');
+// })
+
+const getElement = (tagName) => {
+	const element = document.createElement(tagName);
+
+	return element;
+};
+const createHeader = () => {
+	const header = getElement('header');
+
+	return header;
+};
+
+const movieConstructor = (selector, options) => {
+	const app = document.querySelector(selector);
+
+	if(options.header) {
+		const header = createHeader();
+		app.append(header);
+	}
+	app.textContent = 'Hello World!';
+};
+
+movieConstructor('.app', {
+	title: 'Ведьмак',
+	header: {
+		logo: ''
 	}
 });
-
-const menuButton = document.querySelector('.menu-button');
-const menu = document.querySelector('.header');
-menuButton.addEventListener('click', function () {
-	menuButton.classList.toggle('menu-button-active');
-	menu.classList.toggle('header-active');
-})
