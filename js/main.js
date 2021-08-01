e575761203211e8a09e6069a785a665b298ec7c2
unrealserg <serg.taturin@gmail.com>
2021-08-02 02:06:33 +0300

    Формирование Слайдера

--- js/main.js
+++ js/main.js
@@ -1,27 +1,3 @@
-// new Swiper('.swiper-container', {
-// 	loop: true,
-// 	navigation: {
-// 		nextEl: '.arrow',
-// 	},
-// 	breakpoints: {
-// 		320: {
-// 			slidesPerView: 1,
-// 			spaceBetween: 20
-// 		},
-// 		541: {
-// 			slidesPerView: 2,
-// 			spaceBetween: 40
-// 		}
-// 	}
-// });
-
-// const menuButton = document.querySelector('.menu-button');
-// const menu = document.querySelector('.header');
-// menuButton.addEventListener('click', function () {
-// 	menuButton.classList.toggle('menu-button-active');
-// 	menu.classList.toggle('header-active');
-// })
-
 const getElement = (tagName, classNames, attributes) => {
 	const element = document.createElement(tagName);
 
@@ -127,7 +103,8 @@ const createMain = ({
 		genre,
 		rating,
 		description,
-		trailer
+		trailer,
+		slider
 	}}) => {
 		const main = getElement('main');
 		const container = getElement('div', ['container']);
@@ -206,6 +183,61 @@ const createMain = ({
 			wrapper.append(youtubeImageLink);
 		}
 
+		if(slider) {
+			const sliderBlock = getElement('div', ['series']);
+			const swiperBlock = getElement('div', ['swiper-container']);
+			const swiperWrapper = getElement('div', ['swiper-wrapper']);
+			const arrow = getElement('div', ['arrow']);
+
+			const slides = slider.map((item) => {
+
+				const swiperSlide = getElement('div', ['swiper-slide']);
+				const card = getElement('figure', ['card']);
+				const cardImage = getElement('img', ['card-img'], {
+					src: item.img,
+					alt: ((item.title || '') + ' ' + (item.subtitle || '')).trim()
+				});
+
+				card.append(cardImage);
+
+				if(item.title || item.subtitle) {
+					const cardDescription = getElement('figcaption', ['card-description']);
+					cardDescription.innerHTML = `
+						${item.subtitle ? `<p class="card-subtitle">${item.subtitle}</p>` : ''}
+						${item.title ? `<p class="card-title">${item.title}</p>` : ''}
+					`;
+					
+					card.append(cardDescription);
+				}
+				swiperSlide.append(card);
+
+				return swiperSlide;
+			})
+
+			swiperWrapper.append(...slides);
+			swiperBlock.append(swiperWrapper);
+			sliderBlock.append(swiperBlock, arrow);
+
+			container.append(sliderBlock);
+
+			new Swiper(swiperBlock, {
+				loop: true,
+				navigation: {
+					nextEl: arrow,
+				},
+				breakpoints: {
+					320: {
+						slidesPerView: 1,
+						spaceBetween: 20
+					},
+					541: {
+						slidesPerView: 2,
+						spaceBetween: 40
+					}
+				}
+			});
+		}
+
 		return main;
 }
 
@@ -254,5 +286,27 @@ movieConstructor('.app', {
 		'мешочек чеканных монет этот мужчина избавит вас от всякой настырной нечисти — хоть от чудищ болотных, оборотней и даже' + 
 		'заколдованных принцесс.',
 		trailer: 'https://www.youtube.com/watch?v=P0oJqfLzZzQ',
+		slider: [
+			{
+				img: 'witcher/series/series-1.jpg',
+				title: 'Начало конца',
+				subtitle: 'Серия №1'
+			},
+			{
+				img: 'witcher/series/series-2.jpg',
+				title: 'Четыре марки',
+				subtitle: 'Серия №2'
+			},
+			{
+				img: 'witcher/series/series-3.jpg',
+				title: 'Предательская луна',
+				subtitle: 'Серия №3'
+			},
+			{
+				img: 'witcher/series/series-4.jpg',
+				title: 'Банкеты, ублюдки и похороны',
+				subtitle: 'Серия №4'
+			},
+		]
 	}
 });
