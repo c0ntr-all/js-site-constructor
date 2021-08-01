b764ca23a418306f604d380074840b9e69963a9f
unrealserg <serg.taturin@gmail.com>
2021-08-01 23:34:15 +0300

    Формирование иконок социальных сетей

--- js/main.js
+++ js/main.js
@@ -22,11 +22,16 @@
 // 	menu.classList.toggle('header-active');
 // })
 
-const getElement = (tagName, classNames) => {
+const getElement = (tagName, classNames, attributes) => {
 	const element = document.createElement(tagName);
 
 	if(classNames) {
 		element.classList.add(...classNames);
+	}
+	if(attributes) {
+		for(const attribute in attributes) {
+			element[attribute] = attributes[attribute];
+		}
 	}
 
 	return element;
@@ -37,10 +42,27 @@ const createHeader = (param) => {
 	const wrapper = getElement('div', ['header']);
 
 	if(param.header.logo) {
-		const logo = getElement('img', ['logo']);
-		logo.src = param.header.logo;
-		logo.alt = 'Логотип' + param.title;
+		const logo = getElement('img', ['logo'], {
+			src: param.header.logo,
+			alt: 'Логотип' + param.title,
+		});
 		wrapper.append(logo);
+	}
+
+	if(param.header.social) {
+		const socialWrapper = getElement('div', ['social']);
+		const allSocial = param.header.social.map(item => {
+			const socialLink = getElement('a', ['social-link']);
+			socialLink.append(getElement('img', [], {
+				src: item.image,
+				alt: item.title,
+			}));
+			socialLink.href = item.link;
+
+			return socialLink;
+		});
+		socialWrapper.append(...allSocial);
+		wrapper.append(socialWrapper);
 	}
 
 	header.append(container);
@@ -61,6 +83,23 @@ const movieConstructor = (selector, options) => {
 movieConstructor('.app', {
 	title: 'Ведьмак',
 	header: {
-		logo: 'witcher/logo.png'
-	}
+		logo: 'witcher/logo.png',
+		social: [
+			{
+				title: 'Twitter',
+				link: 'https://twitter.com',
+				image: 'witcher/social/twitter.svg'
+			},
+			{
+				title: 'Instagram',
+				link: 'https://instagram.com',
+				image: 'witcher/social/instagram.svg'
+			},
+			{
+				title: 'Facebook',
+				link: 'https://facebook.com',
+				image: 'witcher/social/facebook.svg'
+			}
+		]
+	},
 });
