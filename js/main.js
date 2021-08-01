65407781e551566028c91c4b114a1162a31e3007
unrealserg <serg.taturin@gmail.com>
2021-08-01 23:59:48 +0300

    Подготовка данных фильма для вывода

--- js/main.js
+++ js/main.js
@@ -95,6 +95,10 @@ const movieConstructor = (selector, options) => {
 	if(options.header) {
 		app.append(createHeader(options));
 	}
+
+	if(options.main) {
+		app.append(createMain(options));
+	}
 };
 
 movieConstructor('.app', {
@@ -134,4 +138,12 @@ movieConstructor('.app', {
 			},
 		]
 	},
+	main: {
+		genre: '2019, фэнтези',
+		rating: 8,
+		description: 'Ведьмак Геральт, мутант и убийца чудовищ, на своей верной лошади по кличке Плотва путешествует по Континенту. За тугой' + 
+		'мешочек чеканных монет этот мужчина избавит вас от всякой настырной нечисти — хоть от чудищ болотных, оборотней и даже' + 
+		'заколдованных принцесс.',
+		trailer: 'https://www.youtube.com/watch?v=P0oJqfLzZzQ',
+	}
 });
